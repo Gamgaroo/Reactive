9e439ec1aad5fc57375928b94e645db1e6497a1a
David Orbelian <david.orbelian@gmail.com>
2020-05-11 23:09:34 +0400

    Add Runtime Reactive Implementations.

---
+++ Runtime/ReactiveDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Gamgaroo.Reactive.Runtime.Abstractions;
+
+namespace Gamgaroo.Reactive.Runtime
+{
+    public sealed class ReactiveDictionary<TKey, TValue> : IReactiveDictionary<TKey, TValue>
+    {
+        public ReactiveDictionary()
+        {
+            _dictionary = new Dictionary<TKey, TValue>();
+        }
+
+        public ReactiveDictionary(IDictionary<TKey, TValue> dictionary)
+        {
+            _dictionary = new Dictionary<TKey, TValue>(dictionary);
+        }
+
+        private readonly Dictionary<TKey, TValue> _dictionary;
+
+        public void Add(TKey key, TValue value)
+        {
+            _dictionary.Add(key, value);
+
+            OnItemAdded?.Invoke(key, value);
+        }
+
+        public void Remove(TKey key)
+        {
+            var value = _dictionary[key];
+
+            _dictionary.Remove(key);
+
+            OnItemRemoved?.Invoke(key, value);
+        }
+
+        public IReadOnlyDictionary<TKey, TValue> Items => _dictionary;
+
+        public event Action<TKey, TValue> OnItemAdded;
+        public event Action<TKey, TValue> OnItemRemoved;
+    }
+}
---
+++ Runtime/ReactiveProperty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Gamgaroo.Reactive.Runtime.Abstractions;
+
+namespace Gamgaroo.Reactive.Runtime
+{
+    public sealed class ReactiveProperty<T> : IReactiveProperty<T>
+    {
+        public ReactiveProperty()
+        {
+        }
+
+        public ReactiveProperty(T value)
+        {
+            _value = value;
+        }
+
+        private T _value;
+
+        public T Value
+        {
+            get => _value;
+            set => SetValue(value);
+        }
+
+        public event Action<T> OnValueChanged;
+
+        private void SetValue(T value)
+        {
+            if (_value != null && _value.Equals(value))
+                return;
+
+            _value = value;
+
+            OnValueChanged?.Invoke(_value);
+        }
+    }
+}
