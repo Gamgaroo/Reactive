59defcb70a7d2bb08bf48d0e50b9d440c48d1b18
David Orbelian <david.orbelian@gmail.com>
2020-05-11 23:04:39 +0400

    Add Runtime Reactive Abstractions.

---
+++ Runtime/Abstractions/IReactiveDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gamgaroo.Reactive.Runtime.Abstractions
+{
+    public interface IReactiveDictionary<TKey, TValue>
+    {
+        void Add(TKey key, TValue value);
+        void Remove(TKey key);
+
+        IReadOnlyDictionary<TKey, TValue> Items { get; }
+
+        event Action<TKey, TValue> OnItemAdded;
+        event Action<TKey, TValue> OnItemRemoved;
+    }
+}
---
+++ Runtime/Abstractions/IReactiveList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gamgaroo.Reactive.Runtime.Abstractions
+{
+    public interface IReactiveList<T>
+    {
+        void Add(T item);
+        void Remove(T item);
+
+        IReadOnlyList<T> Items { get; }
+
+        event Action<T> OnItemAdded;
+        event Action<T> OnItemRemoved;
+    }
+}
